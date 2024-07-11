42795bcc46308c735be07051e88fab5cba283de6
COSMONAUT <56477503+0xCSMNT@users.noreply.github.com>
2024-07-11 18:28:07 +0200

    tokens standards imported, compiles

---
+++ src/My4626.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.20;
+
+import {ERC4626} from "lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC4626.sol";
+import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {IERC20Metadata} from "lib/openzeppelin-contracts/contracts/interfaces/IERC20Metadata.sol";
+
+contract ERC4626Vault is ERC4626 {
+    constructor(
+        address _asset,
+        string memory _name,
+        string memory _symbol
+    ) ERC20(_name, _symbol) ERC4626(IERC20Metadata(_asset)) {}
+}
