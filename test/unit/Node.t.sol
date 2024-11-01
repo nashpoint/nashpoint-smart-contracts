c5805ae4d848d36f2fa7bb021e5715f58b796878
COSMONAUT <56477503+0xCSMNT@users.noreply.github.com>
2024-11-01 19:39:42 +0000

    checkpoint

--- test/unit/Node.t.sol
+++ test/unit/Node.t.sol
@@ -13,11 +13,16 @@ contract NodeTest is BaseTest {
     INode newNode;
     address manager_;
     address escrow_;
+    address rebalancer_;
+    address quoter_;
 
     function setUp() public override {
         super.setUp();
         manager_ = makeAddr("manager_");
         escrow_ = makeAddr("escrow_");
+        rebalancer_ = makeAddr("rebalancer_");
+        quoter_ = makeAddr("quoter");
+
         address[] memory routers = new address[](1);
         routers[0] = address(deployer.erc4626Router());
 
@@ -25,32 +30,24 @@ contract NodeTest is BaseTest {
             nodeFactory.createNode("NewNode", "NN", address(erc20), owner, rebalancer, address(quoter), routers, SALT);
     }
 
-    function test_initialize_alreadyInitialized() public {
-        assertTrue(node.isInitialized());
-
+    function test_initialize_reverts() public {
         vm.prank(owner);
         vm.expectRevert(ErrorsLib.AlreadyInitialized.selector);
         node.initialize(address(escrow), address(queueManager));
-    }
-
-    function test_initialize_zeroAddress() public {
-        assertFalse(newNode.isInitialized());
 
         vm.startPrank(owner);
         vm.expectRevert(ErrorsLib.ZeroAddress.selector);
         newNode.initialize(address(0), manager_);
-
         vm.expectRevert(ErrorsLib.ZeroAddress.selector);
         newNode.initialize(escrow_, address(0));
-    }
+        vm.stopPrank();
 
-    function test_initialize_notOwner() public {
         vm.prank(randomUser);
         vm.expectRevert(abi.encodeWithSignature("OwnableUnauthorizedAccount(address)", randomUser));
         newNode.initialize(escrow_, manager_);
     }
 
-    function test_initialize_success() public {
+    function test_initialize_succeeds() public {
         assertFalse(newNode.isInitialized());
 
         vm.prank(owner);
@@ -61,5 +58,100 @@ contract NodeTest is BaseTest {
         assertTrue(newNode.isInitialized());
         assertEq(newNode.escrow(), escrow_);
         assertEq(address(newNode.manager()), manager_);
+    }
+
+    function test_addRouter_reverts() public {
+        address router = makeAddr("router");
+
+        vm.startPrank(owner);
+        node.addRouter(router);
+        vm.expectRevert(ErrorsLib.AlreadySet.selector);
+        node.addRouter(router);
+        vm.stopPrank();
+
+        vm.prank(owner);
+        vm.expectRevert(ErrorsLib.ZeroAddress.selector);
+        node.addRouter(address(0));
+    }
+
+    function test_addRouter_succeeds() public {
+        address router = makeAddr("router");
+        assertFalse(node.isRouter(router));
+
+        vm.prank(owner);
+        vm.expectEmit(true, false, false, true);
+        emit EventsLib.AddRouter(router);
+        node.addRouter(router);
+
+        assertTrue(node.isRouter(router));
+    }
+
+    function test_removeRouter() public {
+        address router = makeAddr("router");
+
+        vm.prank(owner);
+        vm.expectRevert(ErrorsLib.NotSet.selector);
+        node.removeRouter(router);
+
+        vm.startPrank(owner);
+        node.addRouter(router);
+        assertTrue(node.isRouter(router));
+
+        vm.expectEmit(true, false, false, true);
+        emit EventsLib.RemoveRouter(router);
+        node.removeRouter(router);
+        vm.stopPrank();
+
+        assertFalse(node.isRouter(router));
+    }
+
+    function test_addRebalancer_alreadySet() public {
+        vm.startPrank(owner);
+        node.setRebalancer(rebalancer_);
+        vm.expectRevert(ErrorsLib.AlreadySet.selector);
+        node.setRebalancer(rebalancer_);
+        vm.stopPrank();
+    }
+
+    function test_addRebalancer_suceeds() public {
+        vm.prank(owner);
+        vm.expectEmit(true, false, false, true);
+        emit EventsLib.SetRebalancer(rebalancer_);
+        node.setRebalancer(rebalancer_);
+
+        assertEq(node.rebalancer(), rebalancer_);
+    }
+
+    function test_setEscrow_reverts() public {
+        vm.prank(owner);
+        vm.expectRevert(ErrorsLib.ZeroAddress.selector);
+        node.setEscrow(address(0));
+
+        vm.startPrank(owner);
+        node.setEscrow(escrow_);
+        vm.expectRevert(ErrorsLib.AlreadySet.selector);
+        node.setEscrow(escrow_);
+        vm.stopPrank();
+    }
+
+    function test_setQuoter_reverts() public {
+        vm.prank(owner);
+        vm.expectRevert(ErrorsLib.ZeroAddress.selector);
+        node.setQuoter(address(0));
+
+        vm.startPrank(owner);
+        node.setQuoter(quoter_);
+        vm.expectRevert(ErrorsLib.AlreadySet.selector);
+        node.setQuoter(quoter_);
+        vm.stopPrank();
+    }
+
+    function test_setQuoter_succeeds() public {
+        vm.prank(owner);
+        vm.expectEmit(true, false, false, true);
+        emit EventsLib.SetQuoter(quoter_);
+        node.setQuoter(quoter_);
+
+        assertEq(address(node.quoter()), quoter_);
     }
 }
