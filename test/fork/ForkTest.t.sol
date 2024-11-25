e5dda6741c95fa116dc441635e67d9aae712a341
COSMONAUT <56477503+0xCSMNT@users.noreply.github.com>
2024-11-25 10:03:03 +0000

    usdc address

--- test/fork/ForkTest.t.sol
+++ test/fork/ForkTest.t.sol
@@ -8,7 +8,7 @@ import {console2} from "forge-std/console2.sol";
 
 contract ForkTest is BaseTest {
     uint256 arbitrumFork;
-    address constant usdc = 0xaf88d065e77c8cC2239327C5EDb3A432268e5831;
+    address constant usdcAddress = 0xaf88d065e77c8cC2239327C5EDb3A432268e5831;
     address constant yUsdcA = 0x6FAF8b7fFeE3306EfcFc2BA9Fec912b4d49834C1;
     uint256 constant blockNumber = 277189217;
 
@@ -25,11 +25,11 @@ contract ForkTest is BaseTest {
     }
 
     function test_usdcAddress() public view {
-        string memory name = IERC20Metadata(usdc).name();
-        uint256 totalSupply = IERC20Metadata(usdc).totalSupply();
+        string memory name = IERC20Metadata(usdcAddress).name();
+        uint256 totalSupply = IERC20Metadata(usdcAddress).totalSupply();
         assertEq(name, "USD Coin");
         assertEq(totalSupply, 1808663807522167);
-        assertEq(IERC20Metadata(usdc).decimals(), 6);
+        assertEq(IERC20Metadata(usdcAddress).decimals(), 6);
     }
 
     function test_yearnUsdcA_Address() public view {
@@ -37,6 +37,6 @@ contract ForkTest is BaseTest {
         address vaultAsset = IERC4626(yUsdcA).asset();
 
         assertEq(name, "USDC-A yVault");
-        assertEq(vaultAsset, usdc);
+        assertEq(vaultAsset, usdcAddress);
     }
 }
