4a3a98853764d949ade129c080c638679dbdb088
COSMONAUT <56477503+0xCSMNT@users.noreply.github.com>
2024-12-02 13:53:13 +0400

    removed mint() funtion from 4626 router

--- src/interfaces/IERC4626Router.sol
+++ src/interfaces/IERC4626Router.sol
@@ -14,12 +14,6 @@ interface IERC4626Router is IBaseRouter {
     /// @param assets The amount of assets to deposit.
     function deposit(address node, address vault, uint256 assets) external;
 
-    /// @notice Mints shares from an ERC4626 vault on behalf of the Node.
-    /// @param node The address of the node.
-    /// @param vault The address of the ERC4626 vault.
-    /// @param shares The amount of shares to mint.
-    function mint(address node, address vault, uint256 shares) external;
-
     /// @notice Withdraws assets from an ERC4626 vault on behalf of the Node.
     /// @param node The address of the node.
     /// @param vault The address of the ERC4626 vault.
--- src/routers/ERC4626Router.sol
+++ src/routers/ERC4626Router.sol
@@ -34,18 +34,6 @@ contract ERC4626Router is BaseRouter, IERC4626Router {
         INode(node).execute(vault, 0, abi.encodeWithSelector(IERC4626.deposit.selector, assets, node));
     }
 
-    /// @notice Mints shares from an ERC4626 vault on behalf of the Node.
-    /// @param node The address of the node.
-    /// @param vault The address of the ERC4626 vault.
-    /// @param shares The amount of shares to mint.
-    function mint(address node, address vault, uint256 shares)
-        external
-        onlyNodeRebalancer(node)
-        onlyWhitelisted(vault)
-    {
-        INode(node).execute(vault, 0, abi.encodeWithSelector(IERC4626.mint.selector, shares, node));
-    }
-
     /// @notice Withdraws assets from an ERC4626 vault on behalf of the Node.
     /// @param vault The address of the ERC4626 vault.
     /// @param assets The amount of assets to withdraw.
