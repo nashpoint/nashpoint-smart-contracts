d8eab033b515c9153f6f97370c07de7f981fb4ed
COSMONAUT <56477503+0xCSMNT@users.noreply.github.com>
2025-01-16 15:58:43 -0800

    typo fixed: typo alculates -> calculate

--- src/quoters/QuoterV1.sol
+++ src/quoters/QuoterV1.sol
@@ -79,7 +79,7 @@ contract QuoterV1 is IQuoterV1, BaseQuoter {
                         SWING PRICING FUNCTIONS
     //////////////////////////////////////////////////////////////*/
 
-    /// @dev Called by Node Contract to alculates the deposit bonus
+    /// @dev Called by Node Contract to calculate the deposit bonus
     /// reserveImpact is the inverse of the percentage of the reserve assets shortfall closed by the deposit
     /// The majority of logic is in _calculateReserveImpact and _getSwingFactor
     /// @param asset The asset being deposited
