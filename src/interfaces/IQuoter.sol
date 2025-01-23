adb9d648e8a3ac8bb00fdd02a1e507effd6954a3
COSMONAUT <56477503+0xCSMNT@users.noreply.github.com>
2025-01-22 16:25:46 -0800

    natspec added

--- src/interfaces/IQuoter.sol
+++ src/interfaces/IQuoter.sol
@@ -5,11 +5,24 @@ interface IQuoter {
     /// @notice Returns the total assets in the Node based on valuation of the underlying components
     function getTotalAssets() external view returns (uint256);
 
+    /// @notice Calculates the deposit bonus based on the asset, assets, max swing factor, and target reserve ratio
+    /// @param asset The asset of the Node
+    /// @param assets The assets to deposit
+    /// @param maxSwingFactor The max swing factor of the Node
+    /// @param targetReserveRatio The target reserve ratio of the Node
+    /// @return The deposit bonus
     function calculateDepositBonus(address asset, uint256 assets, uint64 maxSwingFactor, uint64 targetReserveRatio)
         external
         view
         returns (uint256);
 
+    /// @notice Calculates the redeem penalty based on the asset, shares exiting, shares, max swing factor, and target reserve ratio
+    /// @param asset The asset of the Node
+    /// @param sharesExiting The shares exiting
+    /// @param shares The shares to redeem
+    /// @param maxSwingFactor The max swing factor of the Node
+    /// @param targetReserveRatio The target reserve ratio of the Node
+    /// @return The redeem penalty
     function calculateRedeemPenalty(
         address asset,
         uint256 sharesExiting,
