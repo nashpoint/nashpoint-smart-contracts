037ca72851169823ab787dbb68daf12edc4b65ed
ksamarin90 <ksamarin90@gmail.com>
2025-07-24 11:27:36 +0400

    reallocate gearbox

--- test/migrations/AaveVault.t.sol
+++ test/migrations/AaveVault.t.sol
@@ -16,6 +16,7 @@ contract AaveVault is Test {
     address constant RWAFI_OWNER = 0x8d1A519326724b18A6F5877a082aae19394D0f67;
     address constant AAVE_VAULT = 0x8E7617ba208479e1CCA2b929916285C1eCaCe4C5;
     address constant AAVE_HARVEST = 0x803Ae650Bc7c40b03Fe1C33F2a787E81f1c4819c;
+    address constant GEARBOX = 0x890A69EF363C9c7BdD5E36eb95Ceb569F63ACbF6;
 
     INodeRegistry registry = INodeRegistry(0xc3d09B30a04BEBb2D942e060de4B4197e94296f7);
     Node rwafi = Node(0x6ca200319A0D4127a7a473d6891B86f34e312F42);
@@ -42,23 +43,28 @@ contract AaveVault is Test {
         vm.stopPrank();
 
         // prepare data for the step 2
-        uint256 harvestShares = IERC4626(AAVE_HARVEST).balanceOf(address(rwafi));
         uint64 initialRebalanceWindow = rwafi.rebalanceWindow();
-        uint256 minAssetsOut = IERC4626(AAVE_HARVEST).previewRedeem(harvestShares) - 1;
-        ComponentAllocation memory c = rwafi.getComponentAllocation(AAVE_HARVEST);
+        uint256 harvestShares = IERC4626(AAVE_HARVEST).balanceOf(address(rwafi));
+        uint256 minAssetsOutHarvest = IERC4626(AAVE_HARVEST).previewRedeem(harvestShares) - 1;
+        uint256 gearboxShares = IERC4626(GEARBOX).balanceOf(address(rwafi));
+        uint256 minAssetsOutGearbox = IERC4626(GEARBOX).previewRedeem(gearboxShares) - 1;
+        ComponentAllocation memory cH = rwafi.getComponentAllocation(AAVE_HARVEST);
+        ComponentAllocation memory cG = rwafi.getComponentAllocation(GEARBOX);
 
         // 2. Step
         vm.startPrank(RWAFI_OWNER);
         // set aave vault with the same config as harvest
-        rwafi.addComponent(AAVE_VAULT, c.targetWeight, c.maxDelta, c.router);
+        rwafi.addComponent(AAVE_VAULT, cH.targetWeight + cG.targetWeight, cH.maxDelta, cH.router);
         // harvest is set to zero
-        rwafi.updateComponentAllocation(AAVE_HARVEST, 0, 0, c.router);
+        rwafi.updateComponentAllocation(AAVE_HARVEST, 0, 0, cH.router);
         // owner allows to rebalance itself
         rwafi.addRebalancer(RWAFI_OWNER);
         // increase rebalancing window to do reallocation
         rwafi.setRebalanceWindow(24 hours);
         // remove funds from harvest
-        router.liquidate(address(rwafi), AAVE_HARVEST, harvestShares, minAssetsOut);
+        router.liquidate(address(rwafi), AAVE_HARVEST, harvestShares, minAssetsOutHarvest);
+        // remove funds from gearbox
+        router.liquidate(address(rwafi), GEARBOX, gearboxShares, minAssetsOutGearbox);
         // invest those in aave vault
         // actually it's hard to compute how much shares we get, since we need to compute _computeDepositAmount
         router.invest(address(rwafi), AAVE_VAULT, 0);
@@ -66,6 +72,8 @@ contract AaveVault is Test {
         rwafi.setRebalanceWindow(initialRebalanceWindow);
         // remove harvest component
         rwafi.removeComponent(AAVE_HARVEST, false);
+        // remove gearbox component
+        rwafi.removeComponent(GEARBOX, false);
         vm.stopPrank();
 
         // we should have removed all funds
