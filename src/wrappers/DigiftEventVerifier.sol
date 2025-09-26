f1a3b5168ddcd98f562b19696e3ad248874e727f
ksamarin90 <ksamarin90@gmail.com>
2025-09-26 10:45:13 +0400

    refactor to contract

--- src/wrappers/DigiftEventVerifier.sol
+++ src/wrappers/DigiftEventVerifier.sol
@@ -5,11 +5,13 @@ import {MerkleTrie} from "optimism/libraries/trie/MerkleTrie.sol";
 import {RLPReader} from "optimism/libraries/rlp/RLPReader.sol";
 import {Bytes} from "optimism/libraries/Bytes.sol";
 
-library MPTLib {
-    bytes32 constant SETTLE_SUBSCRIBER_TOPIC =
+contract DigiftEventVerifier {
+    bytes32 public constant SETTLE_SUBSCRIBER_TOPIC =
         keccak256("SettleSubscriber(address,address,address[],uint256[],address[],uint256[],uint256[])");
-    bytes32 constant SETTLE_REDEMPTION_TOPIC =
+    bytes32 public constant SETTLE_REDEMPTION_TOPIC =
         keccak256("SettleRedemption(address,address,address[],uint256[],address[],uint256[],uint256[])");
+
+    mapping(bytes32 => bool) public usedLogs;
 
     struct Args {
         uint256 blockNumber;
@@ -18,7 +20,6 @@ library MPTLib {
         bytes[] proof;
         bytes32 eventSignature;
         address emittingAddress;
-        address investor;
         address securityToken;
         address currencyToken;
     }
@@ -27,17 +28,12 @@ library MPTLib {
         bytes32 blockHash;
         bytes32 receiptsRoot;
         bytes32 logHash;
-        uint256 stTokenAmount;
-        uint256 assetAmount;
         uint256 investorIndex;
         RLPReader.RLPItem[] logs;
         RLPReader.RLPItem[] log;
     }
 
-    function verifySettlementEvent(Args memory args, mapping(bytes32 => bool) storage usedLogs)
-        internal
-        returns (uint256, uint256)
-    {
+    function verifySettlementEvent(Args calldata args) external returns (uint256, uint256) {
         Vars memory vars;
         vars.blockHash = keccak256(args.headerRlp);
         require(blockhash(args.blockNumber) != 0, "missed window");
@@ -73,9 +69,9 @@ library MPTLib {
             if (stToken != args.securityToken) continue;
 
             vars.investorIndex = type(uint256).max;
-            for (uint256 i; i < investorList.length; i++) {
-                if (investorList[i] == args.investor) {
-                    vars.investorIndex = i;
+            for (uint256 j; j < investorList.length; j++) {
+                if (investorList[j] == msg.sender) {
+                    vars.investorIndex = j;
                     break;
                 }
             }
@@ -86,10 +82,8 @@ library MPTLib {
             require(!usedLogs[vars.logHash], "log already used");
             usedLogs[vars.logHash] = true;
 
-            vars.stTokenAmount += quantityList[vars.investorIndex];
-            vars.assetAmount += amountList[vars.investorIndex];
+            return (quantityList[vars.investorIndex], amountList[vars.investorIndex]);
         }
-        return (vars.stTokenAmount, vars.assetAmount);
     }
 
     function _hashLog(bytes32 blockHash, bytes32 receiptsRoot, bytes memory txIndexPath, uint256 logIndex)
@@ -101,6 +95,7 @@ library MPTLib {
     }
 
     function _stripTypedPrefix(bytes memory b) private pure returns (bytes memory out) {
+        require(b.length > 0, "zero bytes");
         uint8 t = uint8(b[0]);
         // EIP-2718 typed receipts: 0x01 (EIP-2930), 0x02 (EIP-1559), 0x03 (EIP-4844), etc.
         if (t == 0x01 || t == 0x02 || t == 0x03) {
