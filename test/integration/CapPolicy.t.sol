0855b5bbbc9b7eb4f484a98d68c7648613aa9ea4
ksamarin90 <ksamarin90@gmail.com>
2025-10-24 16:31:42 +0400

    policy tests

--- test/integration/CapPolicy.t.sol
+++ test/integration/CapPolicy.t.sol
@@ -48,9 +48,36 @@ contract CapPolicyTest is BaseTest {
 
         vm.prank(owner);
         policy.setCap(address(node), cap);
-
         _userDeposits(user, 100e18);
 
         _userDepositsWithRevert(user, 1100e18, abi.encodeWithSelector(CapPolicy.CapExceeded.selector, 200e18));
+    }
+
+    function test_CapPolicy_allowsAtCapBoundary() external {
+        uint256 cap = 500e18;
+        vm.prank(owner);
+        policy.setCap(address(node), cap);
+
+        _userDeposits(user, cap);
+        assertEq(node.totalAssets(), cap);
+
+        _userDepositsWithRevert(user, 1, abi.encodeWithSelector(CapPolicy.CapExceeded.selector, 1));
+    }
+
+    function test_CapPolicy_removeCap() external {
+        uint256 cap = 300e18;
+        vm.prank(owner);
+        policy.setCap(address(node), cap);
+
+        _userDeposits(user, cap);
+        assertEq(node.totalAssets(), cap);
+
+        vm.prank(owner);
+        vm.expectEmit(true, true, true, true);
+        emit CapPolicy.CapChange(address(node), 0);
+        policy.setCap(address(node), 0);
+
+        _userDeposits(user2, 200e18);
+        assertEq(node.totalAssets(), cap + 200e18);
     }
 }
